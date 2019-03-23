a3c7d40dea6e4956eca657dd41e1e5adb31e0e99
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-03-23 21:40:53 +0200

    set up routes and adde templating engines to render html pages

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const path = require('path')
+const hoganMiddleware = require('hogan-middleware')
 
 const app = express()
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'mustache')
+app.engine('mustache', hoganMiddleware.__express)
 
 app.get('/', (req, res, next) => {
     res.send('Hello There!!')
@@ -11,6 +16,11 @@ app.get('/json', (req, res, next) => {
         greeting: 'Hello!!'
     }
     res.json(data)
+})
+
+app.get('/home', (req, res, next) => {
+    res.render('home', null)
+
 })
 
 
