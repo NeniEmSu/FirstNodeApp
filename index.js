708c21dac3606e29285f4f07068f9c8e7f69110d
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-03-23 21:23:01 +0200

    created vies folder and home.mustache and downloaded hjs ans hogan-middleware

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ app.get('/', (req, res, next) => {
     res.send('Hello There!!')
 })
 
+app.get('/json', (req, res, next) => {
+    const data = {
+        greeting: 'Hello!!'
+    }
+    res.json(data)
+})
+
+
+
 app.listen(3000)
