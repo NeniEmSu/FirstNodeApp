a7916c12bd827def3f6f40f54ebcee7a729f49f7
Neni EmSu <35854224+cryptodoct0r@users.noreply.github.com>
2019-03-23 22:34:51 +0200

    set up public static assets css, js and img, moved all routes to a route folder and imported the static assests

--- index.js
+++ index.js
@@ -6,23 +6,27 @@ const app = express()
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'mustache')
 app.engine('mustache', hoganMiddleware.__express)
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res, next) => {
-    res.send('Hello There!!')
-})
+// app.get('/', (req, res, next) => {
+//     res.send('Hello There!!')
+// })
 
-app.get('/json', (req, res, next) => {
-    const data = {
-        greeting: 'Hello!!'
-    }
-    res.json(data)
-})
+// app.get('/json', (req, res, next) => {
+//     const data = {
+//         greeting: 'Hello!!'
+//     }
+//     res.json(data)
+// })
 
-app.get('/home', (req, res, next) => {
-    res.render('home', null)
+// app.get('/home', (req, res, next) => {
+//     res.render('home', null)
 
-})
+// })
 
+const indexRouter = require('./routes/index')
 
+app.use('/', indexRouter)
 
 app.listen(3000)
+console.log('server running on http://localhost:3000')
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    res.send('Hello from Routes Folder!')
+})
+
+router.get('/home', (req, res, next) => {
+    res.render('home', null)
+
+})
+
+router.get('/json', (req, res, next) => {
+    res.json({
+        greeting: 'Hello!!'
+    })
+})
+
+module.exports = router
